c0010c029c18c02a8bb649c3c748f91148e8e70f
msv-akshat <141923320+msv-akshat@users.noreply.github.com>
2023-08-10 16:53:42 +0530

    claculator

---
+++ first.py
@@ -0,0 +1,12 @@
+a=float(input(" Enter value of a : "))
+b=float(input(" Enter value of b : "))
+opp=input(" Enterthe operation : ")
+if(opp == '+'):
+   c=a+b
+if(opp == '-'):
+  c=a-b
+if(opp == '*'):
+  c=a*b
+if(opp == '/'):
+  c=a/b
+print(a," ",opp,' ',b,' = ',c)
